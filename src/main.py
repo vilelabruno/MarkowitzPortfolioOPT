1b22d3ae48a01c64bd78aadc4f5507cefa98e9cd
Bruno Vilela <a16042@bcc.unifal-mg.edu.br>
2020-03-04 17:52:48 -0300

    added return on last month

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
-from pybovespa.bovespa import *
-from pybovespa.stock import *
+import investpy
 
 def main():
     shares = []
@@ -9,11 +8,14 @@ def main():
         shares[len(shares)-1] = shares[len(shares)-1].strip()
         weight = prtf.readline().split(",")
         weight[len(weight)-1] = weight[len(weight)-1].strip()
+    
     for share in shares:
-        bovespa = Bovespa()
-        stock = bovespa.query(share)
-        print(stock.cod, stock.name, stock.last)
-
+        df = investpy.get_stock_historical_data(stock=share,country='brazil',from_date='01/01/2020',to_date='03/02/2020')
+        df.reset_index(inplace=True)
+        op = df["Open"][df["Date"] == "2020-01-01"] 
+        #print(df.tail())
+        cl = df["Close"][df["Date"] == "2020-02-02"]
+        print(op[0] - cl )
 
 #if __name__ == 'main':
 main()
