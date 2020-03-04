8a6cf24b4f24604d50ec3a1826270e0791558982
Bruno Vilela <a16042@bcc.unifal-mg.edu.br>
2020-03-04 15:59:59 -0300

    pybovespa implementation without success

--- src/main.py
+++ src/main.py
@@ -1,5 +1,19 @@
-def main():
-    print("hi")
+from pybovespa.bovespa import *
+from pybovespa.stock import *
 
-if __name__ == 'main':
-    main()
+def main():
+    shares = []
+    weight = []
+    with open("../januaryData/necton.txt") as prtf:
+        shares = prtf.readline().split(",")
+        shares[len(shares)-1] = shares[len(shares)-1].strip()
+        weight = prtf.readline().split(",")
+        weight[len(weight)-1] = weight[len(weight)-1].strip()
+    for share in shares:
+        bovespa = Bovespa()
+        stock = bovespa.query(share)
+        print(stock.cod, stock.name, stock.last)
+
+
+#if __name__ == 'main':
+main()
