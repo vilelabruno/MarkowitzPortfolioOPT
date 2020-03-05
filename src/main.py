739d64ea71d345c12656ac72f87d21c3b5d1705a
Bruno Vilela <a16042@bcc.unifal-mg.edu.br>
2020-03-05 12:01:33 -0300

    added functions and updated api to get hist data

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
-import investpy
+from pandas_datareader import data
 
-def main():
+def marko1():
     shares = []
     weight = []
     with open("../januaryData/necton.txt") as prtf:
@@ -10,12 +10,33 @@ def main():
         weight[len(weight)-1] = weight[len(weight)-1].strip()
     
     for share in shares:
-        df = investpy.get_stock_historical_data(stock=share,country='brazil',from_date='01/01/2020',to_date='03/02/2020')
+        start_date = '2020-01-01'
+        end_date = '2020-03-05'
+        df = data.DataReader(share, 'yahoo', start_date, end_date)
         df.reset_index(inplace=True)
         op = df["Open"][df["Date"] == "2020-01-01"] 
         #print(df.tail())
         cl = df["Close"][df["Date"] == "2020-02-02"]
-        print(op[0] - cl )
+        print(df.head())
+
+def marko2():
+    return True
+def marko3():
+    return True
+def marko4():
+    return True
+def marko5():
+    return True
+def marko6():
+    return True
+def marko7():
+    return True
 
 #if __name__ == 'main':
-main()
+marko1()
+marko2()
+marko3()
+marko4()
+marko5()
+marko6()
+marko7()
