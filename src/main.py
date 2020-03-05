d67db847c0ed5b981b8de4c63294293a8ccc6e5f
Bruno Vilela <a16042@bcc.unifal-mg.edu.br>
2020-03-05 12:21:15 -0300

    created func to read file

--- src/main.py
+++ src/main.py
@@ -1,23 +1,41 @@
 from pandas_datareader import data
 
-def marko1():
+def readFile(stockbroker):
     shares = []
     weight = []
-    with open("../januaryData/necton.txt") as prtf:
+    with open("../januaryData/"+stockbroker+".txt") as prtf:
         shares = prtf.readline().split(",")
         shares[len(shares)-1] = shares[len(shares)-1].strip()
         weight = prtf.readline().split(",")
         weight[len(weight)-1] = weight[len(weight)-1].strip()
-    
+    return shares, weight
+
+def marko1():
+    shares = []
+    monthReturn = []
+    weight = []
+    shares, weight = readFile("necton")
+    start_date = '2019-12-03'
+    end_date = '2020-03-03'
+
     for share in shares:
-        start_date = '2020-01-01'
-        end_date = '2020-03-05'
+        returnArr = []
         df = data.DataReader(share, 'yahoo', start_date, end_date)
         df.reset_index(inplace=True)
-        op = df["Open"][df["Date"] == "2020-01-01"] 
+        op = df["Open"][df["Date"] == "2019-12-02"] 
+        #print(df.tail())
+        cl = df["Close"][df["Date"] == "2020-01-06"]
+        returnArr.append(cl-op)
+        op = df["Open"][df["Date"] == "2020-01-06"] 
         #print(df.tail())
         cl = df["Close"][df["Date"] == "2020-02-02"]
-        print(df.head())
+        returnArr.append(cl-op)
+        op = df["Open"][df["Date"] == "2020-02-03"] 
+        #print(df.tail())
+        cl = df["Close"][df["Date"] == "2020-03-02"]
+        print(cl[58])
+        returnArr.append(cl-op)
+    print(returnArr)
 
 def marko2():
     return True
