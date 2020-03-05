7c4581f8f5c7f11d787eacdaeb64f39d66e8b163
Bruno Vilela <a16042@bcc.unifal-mg.edu.br>
2020-03-05 13:59:00 -0300

    finished first part of extract data from stockbrok

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 from pandas_datareader import data
+import pandas as pd
+import numpy as np
 
 def readFile(stockbroker):
     shares = []
@@ -14,28 +16,46 @@ def marko1():
     shares = []
     monthReturn = []
     weight = []
-    shares, weight = readFile("necton")
+    shares, weight = readFile("xp")
     start_date = '2019-12-03'
     end_date = '2020-03-03'
 
+    returnArr = []
+    df = data.DataReader(shares, 'yahoo', start_date, end_date)
+    df.reset_index(inplace=True)
+    #df[("Open", "TGAR11.SA")][df[("Date","")] == "2020-03-03"] = 131.80 #yahoo finance dont returning correct value here 
+
+    shDf = pd.DataFrame(shares,columns=["share"])
+    shDf.reset_index(inplace=True)
+    shDf["m1R"] = 0
+    shDf["m1P"] = 0
+    shDf["m2R"] = 0
+    shDf["m2P"] = 0
+    shDf["m3R"] = 0
+    shDf["m3P"] = 0
     for share in shares:
-        returnArr = []
-        df = data.DataReader(share, 'yahoo', start_date, end_date)
-        df.reset_index(inplace=True)
-        op = df["Open"][df["Date"] == "2019-12-02"] 
-        #print(df.tail())
-        cl = df["Close"][df["Date"] == "2020-01-06"]
-        returnArr.append(cl-op)
-        op = df["Open"][df["Date"] == "2020-01-06"] 
-        #print(df.tail())
-        cl = df["Close"][df["Date"] == "2020-02-02"]
-        returnArr.append(cl-op)
-        op = df["Open"][df["Date"] == "2020-02-03"] 
-        #print(df.tail())
-        cl = df["Close"][df["Date"] == "2020-03-02"]
-        print(cl[58])
-        returnArr.append(cl-op)
-    print(returnArr)
+        cl1 = df[('Close', share)][df[("Date","")] == "2020-01-03"]
+        op1 = df[('Open', share)][df[("Date","")] == "2019-12-03"]
+        cl2 = df[('Close', share)][df[("Date","")] == "2020-02-05"]
+        op2 = df[('Open', share)][df[("Date","")] == "2020-01-03"]
+        cl3 = df[('Close', share)][df[("Date","")] == "2020-02-05"]
+        op3 = df[('Open', share)][df[("Date","")] == "2020-03-03"]
+        #print(op2.iloc[0])
+        shDf["m1R"][shDf["share"] == share] = cl1.iloc[0] - op1.iloc[0]
+        shDf["m1P"][shDf["share"] == share] = (cl1.iloc[0] - op1.iloc[0]) / df[('Open', share)][df[("Date","")] == "2019-12-03"].iloc[0]
+        shDf["m2R"][shDf["share"] == share] = (cl2.iloc[0] - op2.iloc[0])
+        shDf["m2P"][shDf["share"] == share] = (cl2.iloc[0] - op2.iloc[0]) / df[('Open', share)][df[("Date","")] == "2020-01-03"].iloc[0]
+        shDf["m3R"][shDf["share"] == share] = (cl3.iloc[0] - op3.iloc[0])
+        shDf["m3P"][shDf["share"] == share] = (cl3.iloc[0] - op3.iloc[0]) / df[('Open', share)][df[("Date","")] == "2020-03-03"].iloc[0]
+    shDf["m1E"] = (shDf["m1P"] - shDf["m1P"].sum()) * 100
+    shDf["m2E"] = (shDf["m2P"] - shDf["m2P"].sum()) * 100
+    shDf["m3E"] = (shDf["m3P"] - shDf["m3P"].sum()) * 100
+    shDf["m1EQ"] = shDf["m1E"] **2
+    shDf["m2EQ"] = shDf["m2E"] **2
+    shDf["m3EQ"] = shDf["m3E"] **2
+    shDf["variance"] = (shDf["m1EQ"] +shDf["m2EQ"] +shDf["m3EQ"]) * (1/(3-1))
+    shDf["stdDev"] = shDf["variance"] ** (1/2)
+    print(shDf)
 
 def marko2():
     return True
