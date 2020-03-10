5fcd971bfd94e03f0aa121744257564a81bb8911
Rafael Machado <rafafsmachado@gmail.com>
2020-03-10 09:49:41 -0300

    starting model

--- src/model.py
+++ src/model.py
@@ -1,1 +1,11 @@
 import pulp
+
+def create_model(self):
+    # create the model
+    self.model = pulp.LpProblem("Markowitz's_Modern_Portfolio_Theory", pulp.LpMinimize)
+
+    # decision variables: assets percentage
+    self.x = pulp.LpVariable('x', lowBound=0.0, upBound=1.0, cat='Continuous')
+
+    # objective function
+    self.model += pulp.LpSum()
